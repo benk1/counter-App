df2de1679db1c5ba5fd4f479a8c676233b8ef9ae
benk1 <kakengi_bernard@hotmail.com>
2019-01-15 11:32:22 +0200

    changes

---
+++ public/JSONapi.jsx
@@ -0,0 +1,37 @@
+ const movies = [
+  {
+    "_id": "1",
+    "Genre": "Comedy",
+    "Stock": "6",
+    "Rate": "4.5",
+  },
+    
+   {
+    "_id": "2",
+    "Genre": "Comedy",
+    "Stock": "9",
+    "Rate": "3.5",
+
+   },
+
+   {
+    "_id": "3",
+    "Genre": "Action",
+    "Stock": "2",
+    "Rate": "4.0",
+   },
+
+   {
+    "_id": "4",
+    "Genre": "Thriller",
+    "Stock": "16",
+    "Rate": "3.5",
+   },
+
+   {
+    "_id": "5",
+    "Genre": "Comedy",
+    "Stock": "5",
+    "Rate": "9.5",
+   }
+]
